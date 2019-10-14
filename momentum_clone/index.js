97d86284ebe58d581940b756821f304441f898c7
Juyong Jeong <j.jeong@digipen.edu>
2019-10-14 14:19:54 -0700

    problem

--- momentum_clone/index.js
+++ momentum_clone/index.js
@@ -1,7 +1,44 @@
-//alert('Im working. Im JS, Im beautiful, Im worth it!');
-console.log('Im working. Im JS, Im beautiful, Im worth it!');
-const info = 0
-let a = 221;
-let b = a - 5;
-a = 4
-console.log(b, a);
+//const title = document.getElementById("title");
+console.log(document);
+
+// //alert('Im working. Im JS, Im beautiful, Im worth it!');
+// console.log('Im working. Im JS, Im beautiful, Im worth it!');
+// const info = 0
+// let a = 221;
+// let b = a - 5;
+// a = 4
+// const name = "jaykop"
+// const emoji = "😊"
+
+// //js object
+// const personInfo = {
+//     name:"Jay", 
+//     age:27,
+//     gender:"Male"
+// }
+
+// function sayhello(info) {
+//     console.log("hello" + info.name + "you are" + info.age)
+//     // *******backtick!!!!!!!!
+//     return `Hello ${info.name} you are ${info.age} years old`;
+// }
+
+// // var can have function
+// const greetJay = sayhello(personInfo)
+
+// console.log(b, a, name, emoji);
+// console.log(personInfo);
+// sayhello(personInfo);
+
+// console.log(greetJay)
+
+// const calculator = {
+//     add: function (a, b) { return a+b; },
+//     subtract: function (a, b) {return a-b;},
+//     multiply: function (a, b) {return a*b},
+//     divide: function (a, b) {return a/b},
+//     pow: function(a, b) {return a**b;}
+// }
+
+// console.log(calculator.add(1,1))
+// console.log(calculator.pow(2,3))
