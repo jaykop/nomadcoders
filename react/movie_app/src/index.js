6d20042107815b339952b32b9ee0bbc8d2caaadf
jaykop <jaykop.jy@gmail.com>
2019-12-18 18:12:52 -0800

    jsx & props

--- react/movie_app/src/index.js
+++ react/movie_app/src/index.js
@@ -2,4 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// this is component
+// component returns html
+// jsx = combination of js and html
+
+// react app can render only one component at a time!!!
 ReactDOM.render(<App />, document.getElementById('root'));
