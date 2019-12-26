a6679a64b0897be29d0e762d36d244f05d82ec90
jaykop <jaykop.jy@gmail.com>
2019-12-25 19:11:55 -0800

    moive app - adding routers

--- react/movie_app/src/App.js
+++ react/movie_app/src/App.js
@@ -1,52 +1,13 @@
 import React from 'react';
-import axios from 'axios';
-import Movie from './movie';
+import About from './routes/about';
+import {HashRouter, Route} from "react-router-dom";
 
-// render 
-class App extends React.Component {
+function App () {
 
-  state = {
-    isLoading: true,
-    movies: []
-  };
-
-  getMovies = async () => {
-    const {
-      data: {
-        data: { movies }
-      }
-    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
-    // console.log(movies);
-    this.setState({ movies, isLoading: false });
-  }
-
-  componentDidMount() {
-    this.getMovies();
-  }
-
-  render() {
-    const { isLoading, movies } = this.state;
-    return (
-      <section className="container">
-        {isLoading ? (
-          <div className="loader">
-            <span className="loader__text">Loading...</span>
-          </div>
-        ) : (
-            <div className="movies">
-              {movies.map(movie => (
-              <Movie
-                id={movie.id}
-                year={movie.year}
-                title={movie.title}
-                summary={movie.summary}
-                poster={movie.medium_cover_image} />
-          ))}
-          </div>
-       )}
-      </section>
-    );
-  };
+  return <HashRouter>
+    <Route path="/about" component={About} />
+    
+  </HashRouter>
 }
 
 export default App;
