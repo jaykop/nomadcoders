02a3933b06b6f7572504feb1408b0b88d53d7b94
jaykop <jaykop.jy@gmail.com>
2019-12-25 18:47:02 -0800

    Updates

---
+++ precache-manifest.b650dade6a1a97d20ae13e6a9aa472c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb0ce0e890345e3b37f0ecdc248938c9",
+    "url": "/jaykop/nomadcoders/tree/master/react/movie_app/index.html"
+  },
+  {
+    "revision": "2d7da4fd50f85dd1dc64",
+    "url": "/jaykop/nomadcoders/tree/master/react/movie_app/static/css/main.04fb09bf.chunk.css"
+  },
+  {
+    "revision": "cbc22d1e9f5e8712be39",
+    "url": "/jaykop/nomadcoders/tree/master/react/movie_app/static/js/2.076e7f06.chunk.js"
+  },
+  {
+    "revision": "2d7da4fd50f85dd1dc64",
+    "url": "/jaykop/nomadcoders/tree/master/react/movie_app/static/js/main.531a5535.chunk.js"
+  },
+  {
+    "revision": "dc227463dfadd1aed3f1",
+    "url": "/jaykop/nomadcoders/tree/master/react/movie_app/static/js/runtime-main.fcf66449.js"
+  }
+]);
